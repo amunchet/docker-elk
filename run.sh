77accfb7201643818624f747e997a0562cdaaf3c
Chester Enright <cenright@altamontco.com>
2021-01-08 18:56:56 -0600

    feat: added sentinl plugin to kibana

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 # File to run
-docker-compose -f docker-compose.yml -f extensions/logspout/logspout-compose.yml -f extensions/apm-server/apm-server-compose.yml up --build 
+
+echo "Starting dockers..."
+docker-compose -f docker-compose.yml -f extensions/logspout/logspout-compose.yml -f extensions/apm-server/apm-server-compose.yml up --build -d 
+
+echo "Installing sentinl..."
+
+# This doesn't work with cygwin - run manually
+docker exec docker-elk_kibana_1 "/opt/kibana/bin/kibana-plugin /opt/kibana/bin/kibana-plugin install https://github.com/sirensolutions/sentinl/releases/download/tag-6.2.2/sentinl-v6.2.2.zip"
+
+echo "Restarting kibana..."
+docker restart docker-elk_kibana_1
